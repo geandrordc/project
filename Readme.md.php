d17e9509cb75477fb4302601924760916ec257ef
Geandro Souza Canto <geandrordc@hotmail.com>
2020-08-21 21:30:16 -0300

    Add Readme.php

---
+++ Readme.md.php
@@ -0,0 +1,13 @@
+# Git Course
+
+Este é um repositório teste para ensinar como o Git funciona.
+
+Saiba mais em [willianjusten.com.br](http://willianjusten.com.br)
+
+Gostou do curso? Quer mais? Ajude com uma doação, até um café é válido =)
+
+https://www.paypal.com/br/webapps/mpp/buttons
+
+Adicionei mais essa linha com auxilio do Sublime Text3
+
+Hora e data atual: <?php echo date('d/m/Y \à\s H:i:s'); ?>
